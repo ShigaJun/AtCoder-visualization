ce9b4ccf5686a6ea94c0d88a6bfdb1d9d2dbc820
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-13 02:30:54 +0900

    サンプルユーザーのリストを表示

--- src/components/TargetUserSuggestions.jsx
+++ src/components/TargetUserSuggestions.jsx
@@ -1,15 +1,21 @@
+import Box from '@mui/material/Box';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 
 import SuggestionsFilter from "./SuggestionsFilter"
 
-export default function TargetUserSuggestions({ processedDataList, myParticiptions, myLatestRating, myFirstRating }) {
+export default function TargetUserSuggestions({ myParticiptions, myLatestRating, myFirstRating }) {
     const filteredUserIds = SuggestionsFilter(myParticiptions, myLatestRating, myFirstRating);
+    if (!filteredUserIds) return null;
+
     console.log(filteredUserIds);
-    return(
-        <div>
-            
-        </div>
+
+    return (
+        <Box width={200} heigh={200} sx={{ overflowY: "auto" }}>
+            {filteredUserIds.map((id, idx) => (
+                <ListItemButton key={idx}>{id}</ListItemButton>
+            ))}
+        </Box>
     );
 }
