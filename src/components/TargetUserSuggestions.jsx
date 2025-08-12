441955050792f9540612786a7594cc79d9ced6c5
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-13 08:39:53 +0900

    サンプルIDをinputsリストの末尾に追加するように変更

--- src/components/TargetUserSuggestions.jsx
+++ src/components/TargetUserSuggestions.jsx
@@ -2,21 +2,24 @@ import Box from '@mui/material/Box';
 import ListItemButton from '@mui/material/ListItemButton';
 
 import SuggestionsFilter from "./SuggestionsFilter"
+import { MAX_USERS } from "../constants";
 
 export default function TargetUserSuggestions({ myParticiptions, myLatestRating, myFirstRating, inputs, setInputs }) {
     const filteredUserIds = SuggestionsFilter(myParticiptions, myLatestRating, myFirstRating);
     if (!filteredUserIds) return null;
 
-    const handleClick = (idx, id) => {
-        const newInputs = [...inputs];
-        newInputs[idx] = id;
-        setInputs(newInputs);
+    const handleClick = (id) => {
+        if (inputs.length < MAX_USERS) {
+            const newInputs = [...inputs];
+            newInputs[inputs.length] = id;
+            setInputs(newInputs);
+        };
     };
 
     return (
         <Box width={200} height={200} sx={{ border: 1, overflow: "scroll" }}>
             {filteredUserIds.map((id, idx) => (
-                <ListItemButton key={idx} onClick={() => handleClick(idx, id)}>{id}</ListItemButton>
+                <ListItemButton key={idx} onClick={() => handleClick(id)}>{id}</ListItemButton>
             ))}
         </Box>
     );
